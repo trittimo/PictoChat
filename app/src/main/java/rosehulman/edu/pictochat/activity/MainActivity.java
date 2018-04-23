724f4901a9330f17b3963a533c0fec982662d1de
Michael Trittin <trittimo@rose-hulman.edu>
2018-04-23 01:04:51 -0400

    Don't autofocus the friends filter

--- app/src/main/java/rosehulman/edu/pictochat/activity/MainActivity.java
+++ app/src/main/java/rosehulman/edu/pictochat/activity/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
