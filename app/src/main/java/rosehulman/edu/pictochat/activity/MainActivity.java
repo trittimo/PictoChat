00ab70d751f45305da33007719b680bdf9108b41
Michael Trittin <trittimo@rose-hulman.edu>
2018-04-23 00:59:11 -0400

    Messages icon with interactive content

--- app/src/main/java/rosehulman/edu/pictochat/activity/MainActivity.java
+++ app/src/main/java/rosehulman/edu/pictochat/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package rosehulman.edu.pictochat.activity;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,6 +12,12 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 import rosehulman.edu.pictochat.fragment.FriendsFragment;
 import rosehulman.edu.pictochat.R;
@@ -32,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private TextView mMessageCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +68,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuItem item = menu.findItem(R.id.action_messages);
+        item.setActionView(R.layout.messages_badge);
+        RelativeLayout layout = (RelativeLayout) item.getActionView();
+        this.mMessageCountTextView = layout.findViewById(R.id.message_count_text);
+        ImageButton button = layout.findViewById(R.id.messages_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+                Random random = new Random();
+                mMessageCountTextView.setText(random.nextInt(10) + "");
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
